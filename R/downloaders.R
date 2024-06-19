2a83f4a49c9b7c301ecf3756dfbe53ce1305bb63
Nick <nickrrrr@gmail.com>
2024-06-19 15:40:45 +1000

    add figshare downloader

---
+++ R/downloaders.R
@@ -0,0 +1,124 @@
+# Download inferfaces for each source that Trio supports
+
+figshareDl <- function(ID) {
+  if (!requireNamespace("httr2", quietly = TRUE)) {
+    cli::cli_abort(c(
+      "{.pkg httr2} is required to to get data from {.url figshare.com}.",
+      "i" = "You can get it by running: {.code install.packages('httr2')}"
+    ))
+  }
+
+  API_URL <- "https://api.figshare.com/v2/"
+
+  # get file ID from ID if it is available
+  splitID <- unlist(stringr::str_split(ID, "/"))
+
+  if (length(splitID) == 1) {
+    articleID <- ID
+    fileID <- NULL
+  } else if (length(splitID) == 2) {
+    articleID <- splitID[1]
+    fileID <- splitID[2]
+  } else {
+    cli::cli_abort(c(
+      "Invalid dataset ID for figshare: {ID}",
+      "i" = "Should be either: ",
+      "i" = "\t{.strong ARTICLE_ID} for all files",
+      "i" = "\t{.strong ARTICLE_ID}/{.strong FILE_ID} for a single file"
+    ))
+  }
+
+  # Create request URL
+  requestUrl <- glue::glue(
+    API_URL, "articles/{articleID}/files",
+    ifelse(!is.null(fileID), paste0("/", fileID), "")
+  )
+
+  # Execute request
+  req <- httr2::request(requestUrl)
+  resp <- req |>
+    httr2::req_error(is_error = \(resp) FALSE) |>
+    httr2::req_perform()
+
+  body <- resp |> httr2::resp_body_json()
+
+  # Informative error if API query fails
+  if (resp |> httr2::resp_is_error()) {
+    status <- paste(httr2::resp_status(resp), httr2::resp_status_desc(resp))
+    apiMessage <- stringr::str_split(body$message, "\n")
+
+    errorMessage <- paste0(
+      cli::format_error(c(
+        "API Request to figshare failed. Code: {status}",
+        "i" = "Check the figshare article ID and try again.",
+        "i" = "Figshare API error message:"
+      )),
+      "\n",
+      stringr::str_flatten(
+        unlist(lapply(apiMessage, \(x) paste0("> ", x))),
+        collapse = "\n"
+      )
+    )
+    rlang::abort(message = errorMessage)
+  }
+
+  # check if query returned a single item
+  if (!is.null(fileID) || !is.null(names(body))) {
+    body <- list(body)
+  }
+  # for files with the same name, get the most recent ID
+  datasets <- do.call(rbind, lapply(body, data.frame)) |>
+    dplyr::arrange(dplyr::desc(id)) |>
+    dplyr::group_by(name) |>
+    dplyr::slice(1) |>
+    dplyr::ungroup() |>
+    dplyr::select(c("name", "size", "download_url", "computed_md5", "mimetype"))
+
+  # TODO: Set a cache data directory that can be checked against the hashes.
+  tempFolder <- tempdir()
+  dlLoacations <- paste(tempFolder, datasets$name, sep = "/")
+
+  # check if files already exist
+  alreadyDl <- datasets$name %in% list.files(tempFolder)
+
+  # if the files exist, check their md5 hashes
+  # delete files that need redownloading
+  if (any(alreadyDl)) {
+    validDl <- cli::hash_file_md5(
+      dlLoacations[alreadyDl]
+    ) == datasets$computed_md5[alreadyDl]
+    alreadyDl[validDl] <- alreadyDl[validDl] & validDl
+
+    toReDl <- fs::file_exists(dlLoacations[!alreadyDl])
+    fs::file_delete(names(toReDl[toReDl]))
+  }
+
+  # download datasets which are not available locally
+  if (any(!alreadyDl)) {
+    status <- curl::multi_download(
+      datasets$download_url[!alreadyDl], dlLoacations[!alreadyDl]
+    )
+
+    if (!all(status$success)) {
+      # TODO: Deal with one failed download.
+      cli::cli_abort(c(
+        "One or more downloads failed.",
+        "Dataset names: {datasets[!alreadyDl,][!status$success, 'name']}",
+        "Dataset URLs: {datasets[!alreadyDl,][!status$success, 'download_url']}"
+      ))
+    }
+  }
+  # Check md5 checksums
+  MD5equal <- cli::hash_file_md5(dlLoacations) == datasets$computed_md5
+
+  if (!all(MD5equal)) {
+    # TODO: Inform user on how to redownload the data.
+    cli::cli_warn(c(
+      "Not all MD5 hashes of downloaded data are as expected!",
+      "i" = "Reinitiallising the Trio will redownload corrupted data."
+    )
+    )
+  }
+
+  dlLoacations
+}
