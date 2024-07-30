0a5177b5423df20d74c92cf2201a9f4c31e0da79
littlecabiria <71769896+littlecabiria@users.noreply.github.com>
2024-07-30 13:39:10 +1000

    update survbenchmark metric

--- R/metrics.R
+++ R/metrics.R
@@ -6,38 +6,58 @@ kdeMetric <- function(gs, to_eval) {
 }
 
 harrelCIndexMetric <- function(gs, to_eval) {
-  cli::cli_abort(c(
-    "Harrell's c-index is not supported yet! :("
-  ))
+  # cli::cli_abort(c(
+  #   "Harrell's c-index is not supported yet! :("
+  # ))
+  # gs is survival data with time and event, here assume input only two data columns called time and status
+  # to_eval is the prediction of risk 
+  harrelC1 <- Hmisc::rcorr.cens(-prediction_list[[2]], groundtruth_list[[2]])
+  return(harrelC1["C Index"])
 }
 
-
 beggCIndexMetric <- function(gs, to_eval) {
-  cli::cli_abort(c(
-    "Begg's c-index is not supported yet! :("
-  ))
+  # cli::cli_abort(c(
+  #   "Begg's c-index is not supported yet! :("
+  # ))
+  # gs:list(surv_object[ train_index], surv_object[ test_index])
+  # to_eval: list(predictors[ train_index], predictors[test_index])
+  begg_cindex <- survAUC::BeggC(gs[[1]], gs[[2]],to_eval[[1]], to_eval[[2]])
+  return(begg_cindex)
 }
 
 unoCIndexMetric <- function(gs, to_eval) {
-  cli::cli_abort(c(
-    "Uno's c-index is not supported yet! :("
-  ))
+  # cli::cli_abort(c(
+  #   "Uno's c-index is not supported yet! :("
+  # ))
+  
+  uno_cindex <- survAUC::UnoC(gs[[1]], gs[[2]], to_eval)
+  return(uno_cindex)
+  
 }
 
 ghCIndexMetric <- function(gs, to_eval) {
-  cli::cli_abort(c(
-    "GH's c-index is not supported yet! :("
-  ))
+  # cli::cli_abort(c(
+  #   "GH's c-index is not supported yet! :("
+  # ))
+  gh_cindex <- survAUC::GHCI(to_eval)
+  return(gh_cindex)
 }
 
 brierScoreMetric <- function(gs, to_eval) {
-  cli::cli_abort(c(
-    "Brier's score is not supported yet! :("
-  ))
+  # cli::cli_abort(c(
+  #   "Brier's score is not supported yet! :("
+  # ))
+  time <- gs[[1]][,"time"]
+  brier_score <- survAUC::predErr(gs[[1]], gs[[2]],to_eval[[1]], to_eval[[2]],times=time, type = "brier", int.type = "unweighted")$error
+  return(brier_score)
 }
 
 timeDependentAUCMetric <- function(gs, to_eval) {
-  cli::cli_abort(c(
-    "Time dependent AUC is not supported yet! :("
-  ))
+  # cli::cli_abort(c(
+  #   "Time dependent AUC is not supported yet! :("
+  # ))
+  time <- gs[[1]][,"time"]
+  AUC_CD <- survAUC::AUC.uno(gs[[1]], gs[[2]], to_eval[[2]], time)
+  return(AUC_CD)
 }
+
