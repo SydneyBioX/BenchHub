2a7dd7f378997cd3835216ab00ac520799b8bc80
littlecabiria <71769896+littlecabiria@users.noreply.github.com>
2024-09-23 22:15:21 +1000

    Create RecSys.R

---
+++ R/RecSys.R
@@ -0,0 +1,23 @@
+#' @importFrom R6 R6Class
+NULL
+
+#' A RecSys object
+#' @description An object containing a recommender system for data-driven 
+#'   specific task.
+#' @field evalSummary The evaluation summary is stored by dataframe, where
+#'   each row is the compared identifier, each column is the metric used in 
+#'   the evaluation task and related information.
+#' @field metadata A dataframe to store metadata for the benchmark.
+#'   
+#' @examples
+#' TODO
+#' @export
+RecSys <- R6::R6Class(
+  classname = "RecSys",
+  
+  public = list(
+    evalSummary = NULL,   # Placeholder for evalSummary
+    metadata = NULL
+
+  )
+)
