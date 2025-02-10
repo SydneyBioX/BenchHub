687aad36e7c2233420f963bd9389ac1b7c76bc49
skim4524 <skim4524@uni.sydney.edu.au>
2025-02-10 12:57:24 +1100

    microbiome data added

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Human Gut Microbiome Abundance and Patient Classes
+#' 
+#' Data set consists of a matrix of abundances of 1192 microbial taxa for 575 samples
+#' and a factor vector of classes for Parkinson's disease for the same 575 patients
+#' 
+#' 
+#' @name lubomski_microbiome_data
+#' @aliases measurements classes
+#' @docType data
+#' @format \code{measurements} has a row for each sample and a column for each
+#' taxon. \code{classes} is a factor vector with values 0 representing Healthy Control (HC)
+#' and 1 representing Parkinson's Disease (PD).
+#' @source An R package PD16Sdata, \emph{BMC}, 2023. 
+#' Webpage: \url{https://microbiomejournal.biomedcentral.com/articles/10.1186/s40168-023-01475-4}
+#' @keywords datasets
+NULL
