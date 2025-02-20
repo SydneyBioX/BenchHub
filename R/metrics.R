60d8cf6ebe4554911c66aac25ac1b07406cbff42
Nick <nickrrrr@gmail.com>
2025-02-20 11:25:48 +0100

    Refactor metrics functions for consistency and clarity; rename and adjust visibility of helper functions

--- R/metrics.R
+++ R/metrics.R
@@ -1,79 +1,78 @@
-balAccMetric <- function(auxData, predicted)
-{
+balAccMetric <- function(auxData, predicted) {
   confusionMatrix <- table(auxData, predicted)
   classSizes <- rowSums(confusionMatrix)
   mean(diag(confusionMatrix) / classSizes, na.rm = TRUE)
 }
 
-balErrMetric <- function(auxData, predicted)
-{
+balErrMetric <- function(auxData, predicted) {
   confusionMatrix <- table(auxData, predicted)
   classSizes <- rowSums(confusionMatrix)
   classErrors <- classSizes - diag(confusionMatrix)
   mean(classErrors / classSizes, na.rm = TRUE)
 }
 
-positivesNegatives <- function(auxData, predicted)
-{
+.positivesNegatives <- function(auxData, predicted) {
   confusionMatrix <- table(auxData, predicted)
   truePositives <- diag(confusionMatrix)
   falsePositives <- colSums(confusionMatrix) - truePositives
   falseNegatives <- rowSums(confusionMatrix) - truePositives
   trueNegatives <- sum(truePositives) - truePositives
-  list(TP = truePositives, FP = falsePositives,
-       FN = falseNegatives, TN = trueNegatives)
+  list(
+    TP = truePositives, FP = falsePositives,
+    FN = falseNegatives, TN = trueNegatives
+  )
 }
 
-microPrecMetric <- function(auxData, predicted)
-{
-  PN <- positivesNegatives(auxData, predicted)
+microPrecMetric <- function(auxData, predicted) {
+  PN <- .positivesNegatives(auxData, predicted)
   sum(PN[["TP"]]) / sum(PN[["TP"]] + PN[["FP"]])
 }
 
-microRecMetric <- function(auxData, predicted)
-{
-  PN <- positivesNegatives(auxData, predicted)
+microRecMetric <- function(auxData, predicted) {
+  PN <- .positivesNegatives(auxData, predicted)
   sum(PN[["TP"]]) / sum(PN[["TP"]] + PN[["FN"]])
 }
 
-microF1Metric <- function(auxData, predicted)
-{
-  2 * microPrec(auxData, predicted) * microRec(auxData, predicted) /
-     (microPrec(auxData, predicted) + microRec(auxData, predicted))
+microF1Metric <- function(auxData, predicted) {
+  2 * microPrecMetric(auxData, predicted) * microRecMetric(auxData, predicted) /
+    (microPrecMetric(auxData, predicted) + microRecMetric(auxData, predicted))
 }
 
 
-macroPrecMetric <- function(auxData, predicted)
-{
-  PN <- positivesNegatives(auxData, predicted)
+macroPrecMetric <- function(auxData, predicted) {
+  PN <- .positivesNegatives(auxData, predicted)
   sum(PN[["TP"]] / (PN[["TP"]] + PN[["FP"]])) / length(levels(auxData))
 }
 
-macroRecMetric <- function(auxData, predicted)
-{
-  PN <- positivesNegatives(auxData, predicted)
+macroRecMetric <- function(auxData, predicted) {
+  PN <- .positivesNegatives(auxData, predicted)
   sum(PN[["TP"]] / (PN[["TP"]] + PN[["FN"]])) / length(levels(auxData))
-  sum(truePositives / (truePositives + falseNegatives)) / length(levels(auxData))
 }
 
-macroF1Metric <- function(auxData, predicted)
-{
+macroF1Metric <- function(auxData, predicted) {
   2 * macroPrec(auxData, predicted) * macroRec(auxData, predicted) /
-      (macroPrec(auxData, predicted) + macroRec(auxData, predicted))
+    (macroPrec(auxData, predicted) + macroRec(auxData, predicted))
 }
 
-MCCmetric <- function(auxData, predicted)
-{
-  if(length(levels(auxData)) > 2)
-    stop("Error: Matthews Correlation Coefficient calculation ran, but data set has more than two classes.")
-  PN <- positivesNegatives(auxData, predicted)
+MCCmetric <- function(auxData, predicted) {
+  nClass <- length(levels(auxData))
+  if (nClass != 2) {
+    cli::cli_abort(c(
+      "Matthews Correlation Coefficient (MCC) calculation failed.",
+      "i" = "Selected data has {nClass} classes ({.val {levels(auxData)}}).",
+      "i" = "MCC only supports 2 classes."
+    ))
+  }
+  PN <- .positivesNegatives(auxData, predicted)
   (PN[["TP"]][2] * PN[["TN"]][2] - PN[["FP"]][2] * PN[["FN"]][2]) /
-    sqrt((PN[["TP"]][2] + PN[["FP"]][2]) * (PN[["TP"]][2] + PN[["FN"]][2]) * (PN[["TN"]][2] + PN[["FP"]][2]) * (PN[["TN"]][2] + PN[["FN"]][2]))
+    sqrt(
+      (PN[["TP"]][2] + PN[["FP"]][2]) * (PN[["TP"]][2] + PN[["FN"]][2]) *
+        (PN[["TN"]][2] + PN[["FP"]][2]) * (PN[["TN"]][2] + PN[["FN"]][2])
+    )
 }
 
 # One for numeric auxiliary data.
-MSEmetric <- function(auxData, predicted)
-{
+MSEmetric <- function(auxData, predicted) {
   mean((auxData - predicted)^2)
 }
 
@@ -86,53 +85,50 @@ kdeMetric <- function(auxData, predicted) {
 
 #' @importFrom Hmisc rcorr.cens
 harrelCIndexMetric <- function(auxData, predicted) {
+  assertSuggestAvail("Hmisc")
   harrelC1 <- Hmisc::rcorr.cens(-predicted[[2]], auxData[[2]])
   return(harrelC1["C Index"])
 }
 
-#' @import survAUC
+#' @importFrom survAUC BeggC
 beggCIndexMetric <- function(auxData, predicted) {
-  # cli::cli_abort(c(
-  #   "Begg's c-index is not supported yet! :("
-  # ))
-  # auxData:list(surv_object[ train_index], surv_object[ test_index])
-  # predicted: list(predictors[ train_index], predictors[test_index])
-  begg_cindex <- survAUC::BeggC(auxData[[1]], auxData[[2]],predicted[[1]], predicted[[2]])
-  return(begg_cindex)
+  assertSuggestAvail("survAUC")
+
+  survAUC::BeggC(
+    auxData[[1]], auxData[[2]], predicted[[1]], predicted[[2]]
+  )
 }
 
+#' @importFrom survAUC UnoC
 unoCIndexMetric <- function(auxData, predicted) {
-  # cli::cli_abort(c(
-  #   "Uno's c-index is not supported yet! :("
-  # ))
-  
-  uno_cindex <- survAUC::UnoC(auxData[[1]], auxData[[2]], predicted)
-  return(uno_cindex)
-  
+  assertSuggestAvail("survAUC")
+
+  survAUC::UnoC(auxData[[1]], auxData[[2]], predicted)
 }
 
+#' @importFrom survAUC GHCI
 ghCIndexMetric <- function(auxData, predicted) {
-  # cli::cli_abort(c(
-  #   "GH's c-index is not supported yet! :("
-  # ))
-  gh_cindex <- survAUC::GHCI(predicted)
-  return(gh_cindex)
+  assertSuggestAvail("survAUC")
+
+  survAUC::GHCI(predicted)
 }
 
+#' @importFrom survAUC predErr
 brierScoreMetric <- function(auxData, predicted) {
-  # cli::cli_abort(c(
-  #   "Brier's score is not supported yet! :("
-  # ))
-  time <- auxData[[1]][,"time"]
-  brier_score <- survAUC::predErr(auxData[[1]], auxData[[2]],predicted[[1]], predicted[[2]],times=time, type = "brier", int.type = "unweighted")$error
-  return(brier_score)
+  assertSuggestAvail("survAUC")
+
+  time <- auxData[[1]][, "time"]
+  survAUC::predErr(
+    auxData[[1]], auxData[[2]], predicted[[1]], predicted[[2]],
+    times = time, type = "brier", int.type = "unweighted"
+  )$error
 }
 
+#' @importFrom survAUC AUC.uno
 timeDependentAUCMetric <- function(auxData, predicted) {
-  # cli::cli_abort(c(
-  #   "Time dependent AUC is not supported yet! :("
-  # ))
-  time <- auxData[[1]][,"time"]
+  assertSuggestAvail("survAUC")
+
+  time <- auxData[[1]][, "time"]
   AUC_CD <- survAUC::AUC.uno(auxData[[1]], auxData[[2]], predicted[[2]], time)
   return(AUC_CD)
 }
